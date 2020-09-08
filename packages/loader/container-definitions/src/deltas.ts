eb77523f174e7281c5640ffdce66f82e0975339b
Matt Rakow <marakow@microsoft.com>
2020-09-08 16:15:07 -0700

    Add DeltaFeedFollower

--- packages/loader/container-definitions/src/deltas.ts
+++ packages/loader/container-definitions/src/deltas.ts
@@ -177,3 +177,57 @@ export interface IDeltaQueue<T> extends IEventProvider<IDeltaQueueEvents<T>> {
      */
     systemResume(): void;
 }
+
+export interface IDeltaFeedFollowerEvents<T> extends IErrorEvent {
+    (event: "push" | "op", listener: (task: T) => void);
+    (event: "idle", listener: () => void);
+}
+
+export interface IDeltaFeedFollower<T> extends IEventProvider<IDeltaFeedFollowerEvents<T>> {
+    /**
+     * Flag indicating whether or not the queue was paused
+     */
+    paused: boolean;
+
+    /**
+     * The number of messages remaining in the queue
+     */
+    length: number;
+
+    /**
+     * Flag indicating whether or not the queue is idle
+     */
+    idle: boolean;
+
+    /**
+     * Pauses processing on the queue
+     * @returns A promise which resolves when processing has been paused.
+     */
+    pause(): Promise<void>;
+
+    /**
+     * Resumes processing on the queue
+     */
+    resume(): void;
+
+    /**
+     * Peeks at the next message in the queue
+     */
+    peek(): T | undefined;
+
+    /**
+     * Returns all the items in the queue as an array. Does not remove them from the queue.
+     */
+    toArray(): T[];
+
+    /**
+     * System level pause
+     * @returns A promise which resolves when processing has been paused.
+     */
+    systemPause(): Promise<void>;
+
+    /**
+     * System level resume
+     */
+    systemResume(): void;
+}
