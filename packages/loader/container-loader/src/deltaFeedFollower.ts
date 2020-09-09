6753c3fe5563f7b4ba1bd0a3afd5c23b60f85152
Matt Rakow <marakow@microsoft.com>
2020-09-08 17:53:20 -0700

    Some progress on deltaFeedFollower

--- packages/loader/container-loader/src/deltaFeedFollower.ts
+++ packages/loader/container-loader/src/deltaFeedFollower.ts
@@ -14,7 +14,7 @@ export class DeltaFeedFollower extends TypedEventEmitter<IDeltaFeedFollowerEvent
     private readonly sequentialOps: ISequencedDocumentMessage[] = [];
     // Ops that we've received from the future (ahead of the expected sequence number)
     // The feed follower should go find out what the missing ops are so we can complete the sequence.
-    private readonly disjointOps: ISequencedDocumentMessage[] = [];
+    private disjointOps: ISequencedDocumentMessage[] = [];
     // To determine whether the next op we see is sequential or disjoint, we'll see if it's one more than
     // the latestProcessedOpSequenceNumber.
     private latestProcessedOpSequenceNumber: number = 0;
@@ -27,10 +27,12 @@ export class DeltaFeedFollower extends TypedEventEmitter<IDeltaFeedFollowerEvent
         console.log(this.deltaFeed, this.deltaStorage, this.sequentialOps, this.disjointOps);
         // Consider pushing this down - can we start ignoring the documentId arg at the feed level?
         // And unpacking the array
-        deltaFeed.on("op", (documentId, ops) => { this.processIncomingOps(ops); });
+        deltaFeed.on("op", (documentId, ops) => { this.processOps(ops); });
     }
 
-    private processIncomingOps(ops: ISequencedDocumentMessage[]) {
+    // Currently this is expecting to be called either in response to the op event, after retrieving missing ops from
+    // storage, or when reattempting the disjoint ops after processing those missing ops
+    private processOps(ops: ISequencedDocumentMessage[]) {
         for (const op of ops) {
             assert.strict(
                 op.sequenceNumber > this.latestProcessedOpSequenceNumber,
@@ -40,14 +42,29 @@ export class DeltaFeedFollower extends TypedEventEmitter<IDeltaFeedFollowerEvent
                 this.sequentialOps.push(op);
                 this.latestProcessedOpSequenceNumber = op.sequenceNumber;
             } else if (op.sequenceNumber >= this.latestProcessedOpSequenceNumber + 1) {
+                // this is wrong
                 this.disjointOps.push(op);
                 // go fetch the missing ops
+                this.fetchMissingOps(op.sequenceNumber);
                 // maybe proactively push the remaining ops into disjoint since they're all presumably later?
             }
         }
     }
 
-    private fetchMissingOps(to: number) {
-        
+    // If we see an op come in that is in the "future", we will try to get the ops we "missed" from storage.
+    // After getting them, we will process those ops first, and then the "future ops" (which at that point will
+    // hopefully be "present ops").
+    private async fetchMissingOps(to: number) {
+        const missingOps = await this.deltaStorage.get(this.latestProcessedOpSequenceNumber, to);
+        this.processOps(missingOps);
+        this.processDisjointOps();
+    }
+
+    // Empty the disjoint op buffer and run them through processing again.  If there is more disjointedness we might
+    // have to go through again.
+    private processDisjointOps() {
+        const disjointOps = this.disjointOps;
+        this.disjointOps = [];
+        this.processOps(disjointOps);
     }
 }
