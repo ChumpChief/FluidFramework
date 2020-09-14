bd2c7aec50ec6f5360c2e0f7e22a578167c09468
Matt Rakow <marakow@microsoft.com>
2020-09-14 13:25:10 -0700

    Remove getContainer

--- examples/driver-use/src/getTinyliciousContainer.ts
+++ examples/driver-use/src/getTinyliciousContainer.ts
@@ -9,7 +9,6 @@ import {
 } from "@fluidframework/container-definitions";
 import { Container, Loader } from "@fluidframework/container-loader";
 import {
-    IDocumentServiceFactory,
     IFluidResolvedUrl,
     IResolvedUrl,
     IUrlResolver,
@@ -70,46 +69,6 @@ class InsecureTinyliciousUrlResolver implements IUrlResolver {
 
         return jwt.sign(claims, "12345");
     }
-}
-
-export async function getContainer(
-    documentId: string,
-    createNew: boolean,
-    request: IRequest,
-    urlResolver: IUrlResolver,
-    documentServiceFactory: IDocumentServiceFactory,
-    containerRuntimeFactory: IRuntimeFactory,
-): Promise<Container> {
-    const module = { fluidExport: containerRuntimeFactory };
-    const codeLoader = { load: async () => module };
-
-    const loader = new Loader(
-        urlResolver,
-        documentServiceFactory,
-        codeLoader,
-        {},
-        {},
-        new Map(),
-    );
-
-    let container: Container;
-
-    if (createNew) {
-        // We're not actually using the code proposal (our code loader always loads the same module regardless of the
-        // proposal), but the Container will only give us a NullRuntime if there's no proposal.  So we'll use a fake
-        // proposal.
-        container = await loader.createDetachedContainer({ package: "no-dynamic-package", config: {} });
-        await container.attach({ url: documentId });
-    } else {
-        // Request must be appropriate and parseable by resolver.
-        container = await loader.resolve(request);
-        // If we didn't create the container properly, then it won't function correctly.  So we'll throw if we got a
-        // new container here, where we expect this to be loading an existing container.
-        if (!container.existing) {
-            throw new Error("Attempted to load a non-existing container");
-        }
-    }
-    return container;
 }
 
 /**
