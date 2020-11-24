bad70f1ea0ec6f492a91d6b051f4c731a947745d
Matt Rakow <marakow@microsoft.com>
2020-11-24 15:30:07 -0800

    Remove other drivers and tools

--- examples/utils/bundle-size-tests/src/matrix.ts
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-import { SharedMatrix } from "@fluidframework/matrix";
-
-export function apisToBundle() {
-    SharedMatrix.getFactory();
-}
