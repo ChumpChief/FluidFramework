1bdd574581544c36312f98aea0bc242a5ce11f65
Matt Rakow <marakow@microsoft.com>
2020-11-30 14:53:09 -0800

    Remove summary stuff

--- packages/drivers/routerlicious-driver/src/documentStorageService.ts
+++ packages/drivers/routerlicious-driver/src/documentStorageService.ts
@@ -3,19 +3,14 @@
  * Licensed under the MIT License.
  */
 
-import { assert, gitHashFile, IsoBuffer, Uint8ArrayToString } from "@fluidframework/common-utils";
+import { IsoBuffer, Uint8ArrayToString } from "@fluidframework/common-utils";
 import { IDocumentStorageService } from "@fluidframework/driver-definitions";
-import * as resources from "@fluidframework/gitresources";
 import { buildHierarchy } from "@fluidframework/protocol-base";
 import {
-    FileMode,
     ICreateBlobResponse,
     ISnapshotTree,
-    ISummaryTree,
     ITree,
     IVersion,
-    SummaryObject,
-    SummaryType,
 } from "@fluidframework/protocol-definitions";
 import * as gitStorage from "@fluidframework/server-services-client";
 
@@ -84,144 +79,5 @@ export class DocumentStorageService implements IDocumentStorageService {
         return iso.byteLength === iso.buffer.byteLength
             ? iso.buffer
             : iso.buffer.slice(iso.byteOffset, iso.byteOffset + iso.byteLength);
-    }
-
-    private async writeSummaryTree(
-        summaryTree: ISummaryTree,
-        /** Entire previous snapshot, not subtree */
-        previousFullSnapshot: ISnapshotTree | undefined,
-    ): Promise<string> {
-        const entries = await Promise.all(Object.keys(summaryTree.tree).map(async (key) => {
-            const entry = summaryTree.tree[key];
-            const pathHandle = await this.writeSummaryTreeObject(key, entry, previousFullSnapshot);
-            const treeEntry: resources.ICreateTreeEntry = {
-                mode: this.getGitMode(entry),
-                path: encodeURIComponent(key),
-                sha: pathHandle,
-                type: this.getGitType(entry),
-            };
-            return treeEntry;
-        }));
-
-        const treeHandle = await this.manager.createGitTree({ tree: entries });
-        return treeHandle.sha;
-    }
-
-    private async writeSummaryTreeObject(
-        key: string,
-        object: SummaryObject,
-        previousFullSnapshot: ISnapshotTree | undefined,
-        currentPath = "",
-    ): Promise<string> {
-        switch (object.type) {
-            case SummaryType.Blob: {
-                return this.writeSummaryBlob(object.content);
-            }
-            case SummaryType.Handle: {
-                if (previousFullSnapshot === undefined) {
-                    throw Error("Parent summary does not exist to reference by handle.");
-                }
-                return this.getIdFromPath(object.handleType, object.handle, previousFullSnapshot);
-            }
-            case SummaryType.Tree: {
-                return this.writeSummaryTree(object, previousFullSnapshot);
-            }
-            case SummaryType.Attachment: {
-                return object.id;
-            }
-
-            default:
-                throw Error(`Unexpected summary object type: "${object.type}".`);
-        }
-    }
-
-    private getIdFromPath(
-        handleType: SummaryType,
-        handlePath: string,
-        previousFullSnapshot: ISnapshotTree,
-    ): string {
-        const path = handlePath.split("/").map((part) => decodeURIComponent(part));
-        if (path[0] === "") {
-            // root of tree should be unnamed
-            path.shift();
-        }
-
-        return this.getIdFromPathCore(handleType, path, previousFullSnapshot);
-    }
-
-    private getIdFromPathCore(
-        handleType: SummaryType,
-        path: string[],
-        /** Previous snapshot, subtree relative to this path part */
-        previousSnapshot: ISnapshotTree,
-    ): string {
-        const key = path[0];
-        if (path.length === 1) {
-            switch (handleType) {
-                case SummaryType.Blob: {
-                    const tryId = previousSnapshot.blobs[key];
-                    if (!tryId) {
-                        throw Error("Parent summary does not have blob handle for specified path.");
-                    }
-                    return tryId;
-                }
-                case SummaryType.Tree: {
-                    const tryId = previousSnapshot.trees[key]?.id;
-                    if (!tryId) {
-                        throw Error("Parent summary does not have tree handle for specified path.");
-                    }
-                    return tryId;
-                }
-                default:
-                    throw Error(`Unexpected handle summary object type: "${handleType}".`);
-            }
-        }
-        return this.getIdFromPathCore(handleType, path.slice(1), previousSnapshot.trees[key]);
-    }
-
-    private async writeSummaryBlob(content: string | Uint8Array): Promise<string> {
-        const { parsedContent, encoding } = typeof content === "string"
-            ? { parsedContent: content, encoding: "utf-8" }
-            : { parsedContent: Uint8ArrayToString(content, "base64"), encoding: "base64" };
-
-        // The gitHashFile would return the same hash as returned by the server as blob.sha
-        const hash = await gitHashFile(IsoBuffer.from(parsedContent, encoding));
-        if (!this.blobsShaCache.has(hash)) {
-            this.blobsShaCache.set(hash, "");
-            const blob = await this.manager.createBlob(parsedContent, encoding);
-            assert(hash === blob.sha, "Blob.sha and hash do not match!!");
-        }
-        return hash;
-    }
-
-    private getGitMode(value: SummaryObject): string {
-        const type = value.type === SummaryType.Handle ? value.handleType : value.type;
-        switch (type) {
-            case SummaryType.Blob:
-            case SummaryType.Attachment:
-                return FileMode.File;
-            case SummaryType.Commit:
-                return FileMode.Commit;
-            case SummaryType.Tree:
-                return FileMode.Directory;
-            default:
-                throw new Error();
-        }
-    }
-
-    private getGitType(value: SummaryObject): string {
-        const type = value.type === SummaryType.Handle ? value.handleType : value.type;
-
-        switch (type) {
-            case SummaryType.Blob:
-            case SummaryType.Attachment:
-                return "blob";
-            case SummaryType.Commit:
-                return "commit";
-            case SummaryType.Tree:
-                return "tree";
-            default:
-                throw new Error();
-        }
     }
 }
