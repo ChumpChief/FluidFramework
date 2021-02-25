eefd598a59619aec458749f17b279462a7a98724
Matt Rakow <marakow@microsoft.com>
2021-02-25 15:56:42 -0800

    Hack demo to use TaskQueue

--- examples/hosts/app-integration/external-views/src/dataObject.ts
+++ examples/hosts/app-integration/external-views/src/dataObject.ts
@@ -4,7 +4,9 @@
  */
 
 import { EventEmitter } from "events";
+import { TaskQueue } from "@fluid-experimental/task-queue";
 import { DataObject, DataObjectFactory } from "@fluidframework/aqueduct";
+import { IFluidHandle } from "@fluidframework/core-interfaces";
 
 /**
  * IDiceRoller describes the public API surface for our dice roller data object.
@@ -29,16 +31,21 @@ export interface IDiceRoller extends EventEmitter {
 // The root is map-like, so we'll use this key for storing the value.
 const diceValueKey = "diceValue";
 
+const taskQueueKey = "taskQueue";
+
 /**
  * The DiceRoller is our data object that implements the IDiceRoller interface.
  */
 export class DiceRoller extends DataObject implements IDiceRoller {
+    private taskQueue: TaskQueue | undefined;
     /**
      * initializingFirstTime is run only once by the first client to create the DataObject.  Here we use it to
      * initialize the state of the DataObject.
      */
     protected async initializingFirstTime() {
         this.root.set(diceValueKey, 1);
+        const taskQueue = TaskQueue.create(this.runtime);
+        this.root.set(taskQueueKey, taskQueue.handle);
     }
 
     /**
@@ -52,6 +59,11 @@ export class DiceRoller extends DataObject implements IDiceRoller {
                 this.emit("diceRolled");
             }
         });
+
+        const taskQueueHandle = this.root.get<IFluidHandle<TaskQueue>>(taskQueueKey);
+        this.taskQueue = await taskQueueHandle?.get();
+        window["taskQueue"] = this.taskQueue;
+        console.log(this.taskQueue);
     }
 
     public get value() {
@@ -72,6 +84,6 @@ export class DiceRoller extends DataObject implements IDiceRoller {
 export const DiceRollerInstantiationFactory = new DataObjectFactory(
     "dice-roller",
     DiceRoller,
-    [],
+    [TaskQueue.getFactory()],
     {},
 );
