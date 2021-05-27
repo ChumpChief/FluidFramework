401a79c2f5a4aa86ac1cce15090156775f311407
Matt Rakow <matt.rakow@gmail.com>
2021-05-27 01:30:00 -0700

    Remove some drivers

--- packages/drivers/replay-driver/src/index.ts
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-
-export * from "./replayController";
-export * from "./replayDocumentService";
-export * from "./replayDocumentServiceFactory";
-export * from "./storageImplementations";
