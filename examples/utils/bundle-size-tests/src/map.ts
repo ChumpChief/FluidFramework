61f1f7752e727e3bfb0a54a5353a50c85fc77b17
Matt Rakow <matt.rakow@gmail.com>
2021-05-27 01:17:23 -0700

    Remove more examples

--- examples/utils/bundle-size-tests/src/map.ts
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-import { SharedMap } from "@fluidframework/map";
-
-export function apisToBundle() {
-    SharedMap.getFactory();
-}
--- examples/utils/bundle-size-tests/src/sharedString.ts
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-import { SharedString } from "@fluidframework/sequence";
-
-export function apisToBundle() {
-    SharedString.getFactory();
-}
