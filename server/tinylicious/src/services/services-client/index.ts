951a18698d6af594dffa2f796c871d8fdb39d62f
Matt Rakow <matt.rakow@gmail.com>
2021-06-03 21:46:56 -0700

    Start pruning

--- server/tinylicious/src/services/services-client/index.ts
+++ server/tinylicious/src/services/services-client/index.ts
@@ -12,6 +12,4 @@ export * from "./interfaces";
 export * from "./restWrapper";
 export * from "./storage";
 export * from "./summaryTreeUploadManager";
-export * from "./utils";
 export * from "./scopes";
-export * from "./promiseTimeout";
