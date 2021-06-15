c31be6756e5880774e8d908c26864f272e37ce24
Matt Rakow <matt.rakow@gmail.com>
2021-06-14 22:21:26 -0700

    Remove unused

--- server/tinylicious/src/server-services-shared/webServer.ts
+++ server/tinylicious/src/server-services-shared/webServer.ts
@@ -7,7 +7,6 @@ import * as http from "http";
 import { AddressInfo } from "net";
 import * as util from "util";
 import * as core from "../server-services-core";
-import { create } from "./socketIoServer";
 
 export type RequestListener = (request: http.IncomingMessage, response: http.ServerResponse) => void;
 
@@ -29,44 +28,5 @@ export class HttpServer implements core.IHttpServer {
 
     public address(): AddressInfo {
         return this.server.address() as AddressInfo;
-    }
-}
-
-export class WebServer implements core.IWebServer {
-    constructor(public httpServer: HttpServer, public webSocketServer: core.IWebSocketServer) {
-    }
-
-    /**
-     * Closes the web server
-     */
-    public async close(): Promise<void> {
-        await Promise.all([
-            this.httpServer.close(),
-            this.webSocketServer ? this.webSocketServer.close() : Promise.resolve()]);
-    }
-}
-
-export class SocketIoWebServerFactory implements core.IWebServerFactory {
-    constructor(private readonly redisConfig: any, private readonly socketIoAdapterConfig?: any) {
-    }
-
-    public create(requestListener: RequestListener): core.IWebServer {
-        // Create the base HTTP server and register the provided request listener
-        const server = http.createServer(requestListener);
-        const httpServer = new HttpServer(server);
-
-        const socketIoServer = create(this.redisConfig, server, this.socketIoAdapterConfig);
-
-        return new WebServer(httpServer, socketIoServer);
-    }
-}
-
-export class BasicWebServerFactory implements core.IWebServerFactory {
-    public create(requestListener: RequestListener): core.IWebServer {
-        // Create the base HTTP server and register the provided request listener
-        const server = http.createServer(requestListener);
-        const httpServer = new HttpServer(server);
-
-        return new WebServer(httpServer, null);
     }
 }
