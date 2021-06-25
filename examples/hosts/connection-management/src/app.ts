147decaad80bf792463df0f5d95f09a8af0b62f3
Matt Rakow <marakow@microsoft.com>
2021-06-25 13:43:19 -0700

    Delete old comments

--- examples/hosts/connection-management/src/app.ts
+++ examples/hosts/connection-management/src/app.ts
@@ -9,13 +9,6 @@ import { DiceRollerContainerRuntimeFactory } from "./containerCode";
 import { IDiceRoller } from "./dataObject";
 import { renderConnectionControls, renderDiceRoller } from "./view";
 
-// In interacting with the service, we need to be explicit about whether we're creating a new document vs. loading
-// an existing one.  We also need to provide the unique ID for the document we are creating or loading from.
-
-// In this app, we'll choose to create a new document when navigating directly to http://localhost:8080.  For the ID,
-// we'll choose to use the current timestamp.  We'll also choose to interpret the URL hash as an existing document's
-// ID to load from, so the URL for a document load will look something like http://localhost:8080/#1596520748752.
-// These policy choices are arbitrary for demo purposes, and can be changed however you'd like.
 let createNew = false;
 if (location.hash.length === 0) {
     createNew = true;
@@ -25,11 +18,6 @@ const documentId = location.hash.substring(1);
 document.title = documentId;
 
 async function start(): Promise<void> {
-    // The getTinyliciousContainer helper function facilitates loading our container code into a Container and
-    // connecting to a locally-running test service called Tinylicious.  This will look different when moving to a
-    // production service, but ultimately we'll still be getting a reference to a Container object.  The helper
-    // function takes the ID of the document we're creating or loading, the container code to load into it, and a
-    // flag to specify whether we're creating a new document or loading an existing one.
     const service = new TinyliciousService();
     const container = await getExperimentalContainer(
         service,
@@ -40,9 +28,7 @@ async function start(): Promise<void> {
     // eslint-disable-next-line @typescript-eslint/dot-notation
     window["container"] = container;
 
-    // TODO get the connection manager off the container, demo some usage like controlling readonly and reconnect
     // Maybe would be better to pass in the stateful connection (so the host can bring their own connection policy)?
-    // const connectionManager = container.connectionManager;
 
     // Since we're using a ContainerRuntimeFactoryWithDefaultDataStore, our dice roller is available at the URL "/".
     const url = "/";
